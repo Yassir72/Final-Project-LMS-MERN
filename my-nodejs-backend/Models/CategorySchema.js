728f45aadaf59251b0e006a17e3817bf33ac6570
Yassir72 <158035070+Yassir72@users.noreply.github.com>
2024-04-01 12:17:55 +0000

    Update CategorySchema.js

--- my-nodejs-backend/Models/CategorySchema.js
+++ my-nodejs-backend/Models/CategorySchema.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const categorySchema= new mongoose.Schema({
-    name :{type : String , required :true},
-    coursesNumber :{type :Number , required :true}
+    name :{type : String , required :true}
 })
 
 const categoryModel = new mongoose.model('category', categorySchema);
