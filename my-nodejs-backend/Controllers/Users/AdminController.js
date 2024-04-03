eb98bd451f50e0d6630556ce9d1c606217305abf
Yassir <yberraddachy@gmail.com>
2024-04-03 11:04:38 +0000

    build you APIs Now

---
+++ my-nodejs-backend/Controllers/Users/AdminController.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+const {AdminModel} = require("../../Models/AdminSchema")
---
+++ my-nodejs-backend/Controllers/Users/InstructorController.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+const {InstructorModel} = require("../../Models/InstructorSchema")
