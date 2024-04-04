5016ca09a9414959c2c55d146d15160a2c3e653e
Yassir72 <158035070+Yassir72@users.noreply.github.com>
2024-04-04 12:16:57 +0000

    Delete my-nodejs-backend/Config/Database.js

--- my-nodejs-backend/Config/Database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose
-  .connect(process.env.URI)
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((error) => {
-    console.log("Error connecting to database: ", error);
-  });
